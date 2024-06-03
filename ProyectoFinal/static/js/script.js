a939340fb6acebea2a594f2c0210e97ce074b902
Mauro D <103150082+mauro-duarte-97@users.noreply.github.com>
2024-06-03 16:01:49 -0300

    last change

--- ProyectoFinal/static/js/script.js
+++ ProyectoFinal/static/js/script.js
@@ -29,4 +29,68 @@ function onSignIn(googleUser) {
 function handleCredentialResponse(resp) {
   console.log('resp', resp);
 }
-  
+
+// Puntaje Tarjeta Opiniones recientes
+const stars = document.querySelectorAll(".star");
+const emojiEl = document.querySelector(".emoji");
+const statusEl = document.querySelector(".status");
+const defaultRatingIndex = 0;
+let currentRatingIndex = 0;
+
+const ratings = [
+  { emoji: "", name: "Give us rating" },
+  { emoji: "😔", name: "Muy Triste" },
+  { emoji: "🙁", name: "Me Enoja" },
+  { emoji: "🙂", name: "Bien" },
+  { emoji: "🤩", name: "¡Espectacular!" },
+  { emoji: "🥰", name: "G.E.N-IAL" }
+];
+
+const checkSelectedStar = (star) => {
+  if (parseInt(star.getAttribute("data-rate")) === currentRatingIndex) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const setRating = (index) => {
+  stars.forEach((star) => star.classList.remove("selected"));
+  if (index > 0 && index <= stars.length) {
+    document
+      .querySelector('[data-rate="' + index + '"]')
+      .classList.add("selected");
+  }
+  emojiEl.innerHTML = ratings[index].emoji;
+  statusEl.innerHTML = ratings[index].name;
+};
+
+const resetRating = () => {
+  currentRatingIndex = defaultRatingIndex;
+  setRating(defaultRatingIndex);
+};
+
+stars.forEach((star) => {
+  star.addEventListener("click", function () {
+    if (checkSelectedStar(star)) {
+      resetRating();
+      return;
+    }
+    const index = parseInt(star.getAttribute("data-rate"));
+    currentRatingIndex = index;
+    setRating(index);
+  });
+
+  star.addEventListener("mouseover", function () {
+    const index = parseInt(star.getAttribute("data-rate"));
+    setRating(index);
+  });
+
+  star.addEventListener("mouseout", function () {
+    setRating(currentRatingIndex);
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  setRating(defaultRatingIndex);
+});
